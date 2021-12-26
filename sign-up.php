2ec220c278115afdf9fb0e45c828817798d07b7d
VuThiNu <vunu.cntt@gmail.com>
2021-12-27 00:28:48 +0700

    Nu

---
+++ sign-up.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/sign-up.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+
+<body>
+    <main>
+        <div class="container row">
+            <div class="left col-md-3">
+                <img src="img/signup-left.jpg" alt="">
+            </div>
+            <div class="right col-md-6">
+                <div class="header mt-5 mb-3">
+                    <h1>Sign up</h1>
+                    <p>By continuing, you are setting up a Reddit account and agree to our
+                      <a href="">User Agreement</a>    and <a href=""> Privacy Policy.</a>
+                    </p>
+                </div>
+                <div class="main-right">
+                    <button class="google mt-3">
+                        <div class="  mb-3">
+                            <i id = "logogg" class="bi bi-google"></i>
+                            <span>CONTINUE WITH GOOGLE</span>
+                        </div>
+                    </button>
+                   <button class=" apple mt-3">
+                    <div >
+                        <i class="bi bi-apple"></i>
+                        <span>CONTINUE WITH APPLE </span>
+                    </div>
+                   </button>
+                   <span class="mt-5 ">
+                       <hr class=" mt-3 ">
+                   </span>
+                    <div class=" email mt-5">
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email">
+                      </div>
+                      
+                      <a href="sign-up1.php"><button class="bt-continue mt-3">CONTINUE</button></a>
+                </div>
+            </div>
+        </div>
+    </main>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ sign-up1.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/sign-up1.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+
+<body>
+    <header>
+        <h1>Choose your username</h1>
+        <p>Your username is how other community members will see you. This name will be used to credit you for things
+            you share on Reddit. What should we call you?</p>
+    </header>
+
+    <main>
+        <hr>
+        <div class="container row">
+            <div class="left col-md-6">
+                <div class=" username mt-5">
+                    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                        placeholder="CHOOSE A USERNAME">
+                </div>
+                <div class=" password mt-5">
+                    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                        placeholder="PASSWORD">
+                </div>
+            </div>
+            <div class=" right col-md-6 mt-5">
+                <span>Here are some username suggestions </span><br>
+                <a href="">Nervous-Crew-4280</a><br>
+                <a href="">Most-Duty-5973</a><br>
+                <a href="">Big_One_3291</a><br>
+                <a href="">BeneficialCookie8515</a><br>
+                <a href="">Professional_Ice5490</a><br>
+            </div>
+        </div>
+        <hr>
+        <div class="bottom ">
+            <div class="back ">
+                <a href="sign-up.php"><button>Back</button></a>
+            </div>
+            <div class="signup ">
+                
+                <a href="gdsignup.php"><button>Sign up</button></a>
+            </div>
+        </div>
+
+    </main>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
